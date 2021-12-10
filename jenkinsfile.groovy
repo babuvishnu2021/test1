8720daa5fe3a4de1960450c88015afcba50e1ee0
babuvishnu2021 <95462470+babuvishnu2021@users.noreply.github.com>
2021-12-10 21:02:46 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,6 +4,19 @@ pipeline {
         stage("A") {
             steps {
                 println "Started stage A"
+                (1..20).findAll{it in ODD}
+                (1..20).each{
+                  switch(it) {
+                    case EVEN:
+                      println "${it} is even"
+                      break
+                    case ODD:
+                      println "${it} is odd"
+                      break
+                    default:
+                      println "${it} is a mystery to me"
+                  }
+                }
             }
         }
         stage("B") {
